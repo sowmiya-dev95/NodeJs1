bb56c7e538448528483ad65868504dbf07d2f006
sowmiya-dev95 <sowmiyarjss95@gmail.com>
2024-09-05 19:57:19 +0530

    app.js file added

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const routes = require('./routes')
+
+console.log(routes.someText);
+console.log('testing');
+
+const server = http.createServer(routes.handler);
+server.listen(4000); 
+ 
